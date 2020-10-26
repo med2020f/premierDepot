3bfe17ddcbed684d06f4966e6144b5a81d03c527
edjekouane <73483961+edjekouane@users.noreply.github.com>
2020-10-26 16:10:31 +0100

    Update Enseignant.cs

--- Enseignant.cs
+++ Enseignant.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Enseignant
+namespace Enseignant_test
 {
     public abstract class Enseignant
     {
